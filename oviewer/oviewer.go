c1f7a9f6e52c2fc91f26e5325ef143e7c8101131
Noboru Saito <noborusai@gmail.com>
2026-02-19 08:42:36 +0900

    Add --sidebar-mode command-line option
    - Add the --sidebar-mode option to specify predefined settings for sidebar mode.
    - Bind the option to the configuration and support shell completion.
    - Update config and initialization to handle SidebarMode.

--- oviewer/oviewer.go
+++ oviewer/oviewer.go
@@ -686,7 +686,7 @@ func (root *Root) SetConfig(config Config) {
 	}
 
 	if config.SidebarMode != "" {
-		for s := SidebarModeHelp; s < SidebarModeEnd; s++ {
+		for s := SidebarModeNone; s < SidebarModeEnd; s++ {
 			if strings.EqualFold(config.SidebarMode, s.String()) {
 				root.sidebarMode = s
 				root.sidebarVisible = true
